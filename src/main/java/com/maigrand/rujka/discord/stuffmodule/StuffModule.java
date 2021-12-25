6d1b5c7b95efc46f3a822cdf1af281dd12532864
mai <mkmaxz48@gmail.com>
2021-12-25 09:30:59 +0300

    feat: добавлен discord stuff module

---
+++ src/main/java/com/maigrand/rujka/discord/stuffmodule/StuffModule.java
@@ -0,0 +1,31 @@
+package com.maigrand.rujka.discord.stuffmodule;
+
+import com.maigrand.rujka.discord.DiscordModule;
+import com.maigrand.rujka.service.JdaService;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StuffModule extends DiscordModule {
+
+    public StuffModule(JdaService jdaService) {
+        super(jdaService);
+    }
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        super.onGuildMessageReceived(event);
+
+        if (event.getMessage().getContentRaw().contains("май") || event.getMessage().getContentRaw().contains("mai")) {
+            if (event.getMessage().getAuthor().isBot()) {
+                return;
+            }
+            event.getJDA().getUserById("154437997989855232").openPrivateChannel().queue(privateChannel -> {
+                privateChannel.sendMessage("Mentioned " + event.getMessage().getJumpUrl()).queue();
+            }, throwable -> {
+                System.out.println(throwable.getMessage());
+            });
+        }
+    }
+}
