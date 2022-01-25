f5bce889b919ba9396e5b389e62f061b72776547
mai <mkmaxz48@gmail.com>
2022-01-25 18:44:57 +0300

    feat: отключение неиспользуемого функционала jda

--- src/main/java/com/maigrand/rujka/service/JdaService.java
+++ src/main/java/com/maigrand/rujka/service/JdaService.java
@@ -3,11 +3,13 @@ package com.maigrand.rujka.service;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.ChunkingFilter;
+import net.dv8tion.jda.api.utils.MemberCachePolicy;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.LoginException;
-import java.util.EnumSet;
 
 @Service
 public class JdaService {
@@ -20,6 +22,25 @@ public class JdaService {
     public JDA getJda() {
         if (this.jda == null) {
             try {
+                JDABuilder jdaBuilder = JDABuilder.createLight(token);
+                jdaBuilder.disableCache(CacheFlag.ACTIVITY);
+                jdaBuilder.setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER));
+                jdaBuilder.setChunkingFilter(ChunkingFilter.NONE);
+                jdaBuilder.disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING);
+                jdaBuilder.setLargeThreshold(50);
+
+                this.jda = jdaBuilder.build();
+            } catch (LoginException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
+        }
+        return this.jda;
+    }
+
+    /*public JDA getJda() {
+        if (this.jda == null) {
+            try {
                 EnumSet<GatewayIntent> gatewayIntents = EnumSet.allOf(GatewayIntent.class);
                 this.jda = JDABuilder.create(token, gatewayIntents).build();
             } catch (LoginException e) {
@@ -28,5 +49,5 @@ public class JdaService {
             }
         }
         return this.jda;
-    }
+    }*/
 }
